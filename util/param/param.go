5d0a6623e24edfc7cd114006b342302c439e6ae5
ZHU HAIHUA <kimiazhu@gmail.com>
2016-02-28 21:52:03 +0800

    refactor

---
+++ util/param/param.go
@@ -0,0 +1,36 @@
+// Description: model
+// Author: ZHU HAIHUA
+// Since: 2016-02-28 20:59
+package param
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+// MustInt try to extract value from request param by the givin key,
+// if failed will raise a panic
+func MustInt(c *gin.Context, key string) int {
+	if strv := c.Param(key); strv != "" {
+		v, e := strconv.Atoi(strv)
+		if e != nil {
+			panic(e)
+		}
+		return v
+	} else {
+		panic(fmt.Sprintf("cannot get value of param [%s]", key))
+	}
+}
+
+// Int try to extract value from request param by the givin key,
+// if failed the error will set and defaults will be returned (if exists)
+func Int(c *gin.Context, key string, defaults ...int) (int, error) {
+	if strv := c.Param(key); strv != "" {
+		return strconv.Atoi(strv)
+	} else if len(defaults) > 0 {
+		return defaults[0], nil
+	} else {
+		return strconv.Atoi(strv)
+	}
+}
